d9aac5d2eba69de4b63514862d598286c7957a11
holtzy <yan.holtz@datadoghq.com>
2019-04-16 09:52:27 +0200

    end of streamgraph + tab logo for all pages

--- img/graph/script_reformat_img.sh
+++ img/graph/script_reformat_img.sh
@@ -1,5 +1,5 @@
 FILE=$1
-convert Capture*.png -resize 480x480 new.png
+convert Screensh*.png -resize 480x480 new.png
 convert -size 480x480 xc:white new.png  -gravity center -composite output.png
 mv output.png $FILE
-rm Capture*.png new.png
+rm Screensh*.png new.png
