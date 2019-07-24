7c373856e7a0f7f80125fc9f7274d999971a4eed
holtzy <yan.holtz@datadoghq.com>
2019-07-24 22:51:34 +0200

    reRun all Rmd

--- script_run_all.R
+++ script_run_all.R
@@ -1,4 +1,6 @@
 fileNames <- Sys.glob("*.Rmd")
 for( i in fileNames){
-	rmarkdown::render(i)
+	tryCatch({
+		rmarkdown::render(i, output_format="html_document", quiet=TRUE)
+	}, error=function(e){print(i)})
 }
